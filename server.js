5a5fc2766c95b605cf2b67ad4d659ec8c3d44076
Stephen Schroeder <schrode50@gmail.com>
2017-11-10 15:31:08 -0800

    little more routing and html done

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var express = require('express');
 var routes = require(__dirname + '/app/routes/index');
 var app = express();
 
+app.use('/public', express.static(__dirname + '/public'));
+app.use('controllers', express.static(__dirname + 'app/controllers'));
+
 routes(app);
 
 app.listen(3000, () => console.log('Listening on 3000'));
